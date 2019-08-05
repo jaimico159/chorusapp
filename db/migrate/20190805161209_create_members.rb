f58115f8c8ae482d4546b66767abc5376ee56aa4
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2019-08-05 11:16:30 -0500

    creates member model

---
+++ db/migrate/20190805161209_create_members.rb
@@ -0,0 +1,14 @@
+class CreateMembers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :members do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+      t.integer :role
+      t.string :email
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
